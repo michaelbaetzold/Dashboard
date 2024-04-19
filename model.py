30245effe9a91ebd7265f9f5a911b144f6aaeabd
Michael Bätzold <michaelbaetzold@web.de>
2024-04-19 15:42:45 +0200

    model classes done without methods

--- model.py
+++ model.py
@@ -1,15 +1,7 @@
 from enum import Enum
 
 
-class Person:
-    name = str
-    email = str
-
-
-class User(Person):
-    gradeGoals = {}
-
-
+#<editor-fold desc="Enums">
 class ExamTypes(Enum):
     WRITTEN_EXAM = 1
     TECHNICAL_PRESENTATION = 2
@@ -23,4 +15,51 @@ class ExamTypes(Enum):
     BACHELOR_THESIS = 10
 
 
+class DegreeTypes(Enum):
+    BACHELORS = 1
+    MASTERS = 2
+    DOCTORATE = 3
 
+#</editor-fold>
+
+
+#<editor-fold desc="Classes">
+class Exam:
+    grade = float
+    exam_type = ExamTypes
+    attempt = int
+    part_of_final_grade = bool
+
+
+class Person:
+    name = str
+    last_name = str
+    email = str
+
+
+class Course:
+    id = str
+    exam = Exam
+    instructor = Person
+    name = str
+    ects = int
+
+
+class Semester:
+    name = str
+    courses = []
+    semesterAverage = float
+
+
+class DegreeProgram:
+    name = str
+    degree = DegreeTypes
+    semester = []
+
+
+class User(Person):
+    grade_goals = {}
+    style = str
+    degree_program = DegreeProgram
+
+#</editor-fold>
